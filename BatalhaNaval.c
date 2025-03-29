5496f2940f435393471dcfe3ac9ad538b1432b2f
JohnOliverMoura <mouraxjohn@gmail.com>
2025-03-29 23:55:44 +0000

    Nivel aventureiro

--- BatalhaNaval.c
+++ BatalhaNaval.c
@@ -1,92 +1,71 @@
 #include <stdio.h>
 
+#define Linhas 10
+#define Colunas 10
+
+
 int main(){
 	
 	//Declarando as variáveis que serão usadas para formar o tabuleiro e as coordenadas
 	
-	int i, j;
+	int matriz[Linhas][Colunas]; //declaração da matriz/ tabuleiro 
+	int i, j, soma=0, mar=0;//a declaração da soma como zero, vai definir que a matriz faça a atribuição dos numeros da matriz em zeros 
 	char letra[10] = {'A','B','C','D','E','F','G','H','I','J'};// Declarando esse vetor para ser o cabeçalho do tabuleiro que formará as coordenadas
 	int numero[10] = {1,2,3,4,5,6,7,8,9,10};//declarando esse vetor para ser a lateral esquerda do tabuleiro que formará as coordenadas
+	int reta; //Variável para declarar as coordenadas dos návios que estão na horizontal e vertical 
 	
 	
-	printf(" ");//Usado para dar um espaço na primeir letra impressa no cabeçalho do tabuleiro 
 	
-	//Usando loops aninhados para imprimir o cabeçalho de letras do tabuleiro que formará as coordenadas
-	for (i= 0; i<10; ++i)
+	printf("   ");//Alinha as letras com os espaçamentos dos numeros
+	for(i=0; i<Linhas; i++)//Esse loop unico mantem as letras do vetor sobre o tabuleiro/matriz como um cabeçalho
 	{
-		printf(" ");
-		printf("%c", letra[i]);
+		printf(" %c", letra[i]);//Imprime o cabeçalho de letras do tabuleiro
 	}
 	
 	
-	printf("\n");//Usado para pular uma linha após a impressão das letras do cabeçalho do tabuleiro 
-	
-	//Declarando a matriz do tabuleiro 
-	int tabuleiro [10][10]= {{0,0,0,0,0,0,0,0,0,0},
-							{0,0,0,0,0,0,0,0,0,0},
-					 		{0,0,0,0,0,0,0,0,0,0},
-							{0,0,0,0,0,0,0,0,0,0},
-			 				{0,0,0,0,0,0,0,0,0,0},
-							{0,0,0,0,0,0,0,0,0,0},
-			 				{0,0,0,0,0,0,0,0,0,0},
-			 				{0,0,0,0,0,0,0,0,0,0},
-							{0,0,0,0,0,0,0,0,0,0},
-			 				{0,0,0,0,0,0,0,0,0,0}};
-					
-
-	
-	//Usando loops aninhados para imprimir o tabuleiro e os numeros 1 a 10 na lateral que formará as coordenadas do tabuleiro.
-		for (i = 0; i<10; i++)
-		{  
-			printf("%d ", numero[i] );//Imprime os numeros de coordenadas laterais 
+	for( i = 0; i<Linhas; i++)//Loop principal. Determina a ordem dos numeros na barra lateral fora do tabuleiro 
+	{ 	
+		printf("\n");
+		printf("%2d ", numero[i]);//Imprime a barra lateral dos numeros de 1 a 10.
+		
+		for( j=0; j<Colunas; j++)//Imprime o tabuleiro/ matriz
+		{
+			matriz[i][j] = mar;//A variavel mar recebeu o valor zero e garante que a minha mtriz zera reproduzida com todas as linhas e colunas com zero 
 			
-			for(j = 0; j<10; ++j)
+			for (soma=0; soma<3; soma++)//Loop que controla a quantidde de casas que serão preenchidas com o número 3 (návio)
 			{
-			printf("%d ", tabuleiro[i][j]);//Imprime o tabuleiro 
+				if(soma == i && soma== j)//Esse if garante que os números 3/návio só serão colocados quando o valor de soma for igual a i e j, o que garante que fiquem na diagonal.Aninhado ao primeiro loop garante que os navios ocupem apenas 3 celulas/espaços 
+				{
+				matriz[i][j]= 3;//Aqui indico o que a matriz deve substituir 	
+				}
+				
+				if(i + j == 8 && soma ==j )//O i+j == 8 determinam a linha onde o 3 vai ser agregado e a soma == j garante o fim da adição
+				{
+					matriz[i][j]=3;
+				}
+				
 			}
-		 	printf("\n");//Diagrama o tabuleiro 
-	
-		}
-
-
-	//Declarando a posição do navio vertical 
-	tabuleiro[0][1] = 3;
-	tabuleiro[1][1] = 3;
-	tabuleiro[2][1] = 3;
-
-	
-	//Declarando a posição do navio horizontal
-	tabuleiro[3][3] = 3;
-	tabuleiro[3][4] = 3;
-	tabuleiro[3][5] = 3;
-
-
-	printf("\n");
-
-	printf(" ");//Ajuste das letras do tabuleiro 
-	for (i= 0; i<10; ++i)
-	{
-		printf(" ");//Separação entre as letras do cabeçalho 
-		printf("%c", letra[i]);
-	}
-	
-	
-	printf("\n");
-	
-	//Loops aninhados para imprimir o tabuleiro e os numeros 1 a 10 na lateral que formará as coordenadas do tabuleirojá com os navios posicionados 
-		for (i = 0; i<10; i++)
-		{  
-			printf("%d ", numero[i] );//Imprime os numeros de coordenadas laterais
 			
-			for(j = 0; j<10; ++j)
-			{
-			printf("%d ", tabuleiro[i][j]);//Imprime o tabuleiro já com os navios posicionados 
+			for(reta =0; reta<3; reta++)// Loop que define as posições dos navios em linha reta (horizontal e vertical), também define o fim da aplicação 
+			{	
+				if ( reta == i  && j==9)// j==9 garante que a linha nove receba o numero 3. Reta ==i determina onde deve parar a adição, devido ao operador lógico &&.
+				{
+					matriz[i][j] = 3;
+				}
+			
+				
+				if( reta == j && i ==4 )//Esse if determina que quando i for igual a 4 toda a linha recebe o valor 3 e Reta == j determina onde parar devido ao operador logico &&.
+				{
+					matriz[i][j] = 3;
+				}
 			}
-		 	printf("\n");//Diagrama o tabuleiro 
-	
+				
+			printf("%2d", matriz[i][j]); //Imprime a matriz completa 
 		}
-
-
+		
+	}	printf("\n");
+	
+	
 
 	return 0;
 
